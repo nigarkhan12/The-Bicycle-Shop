501375764f27de2653733c8a641cda216175e236
nigarkhan12 <nigarkhan92@gmail.com>
2022-08-21 18:27:44 +0530

    Rendering CSS, Images, SVG Files

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const http = require('http');
 const url = require('url');
+const fs = require('fs').promises;
 
-const server = http.createServer((req, res) => {
+
+const server = http.createServer(async (req, res) => {
     console.log('Server is now running!');
 
     if (req.url === 'favicon.ico') return;
@@ -12,11 +14,36 @@ const server = http.createServer((req, res) => {
     const pathname = myURL.pathname;
     const id = myURL.searchParams.get('id');
 
-    console.log(req.headers);
-    console.log(pathname, id);
+    if (pathname === '/') {
+        const html = await fs.readFile('./view/bicycles.html', 'utf-8');
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(html);
+    }
+    else if (pathname === '/bicycle' && id>=0 && id<=5) {
+        const html = await fs.readFile('./view/overview.html', 'utf-8');
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(html);
+    }
+    else if (/\.(png)$/i.test(req.url)) {
+        const image = await fs.readFile(`./public/images/${req.url.slice(1)}`);
+        res.writeHead(200, { 'Content-Type': 'image/png' });
+        res.end(image);
+    }
+    else if (/\.(css)$/i.test(req.url)) {
+        const css = await fs.readFile(`./public/css/index.css`);
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.end(css);
+    }
+    else if (/\.(svg)$/i.test(req.url)) {
+        const svg = await fs.readFile(`./public/images/icons.svg`);
+        res.writeHead(200, { 'Content-Type': 'image/svg+xml' });
+        res.end(svg);
+    }
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('<div><h1>File Not Found</h1></div>');
+    }
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<h1>Welcome to the Bicycle Shop</h1>');
 });
 
 
